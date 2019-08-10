d299e1357e6a68a2366e4929c584287c12065333
Nilesh Pawar <nilesh.p.pawar@gmail.com>
2019-08-10 19:35:39 +0100

    updated example 1

--- src/test/java/MyDummyApplicationTest.java
+++ src/test/java/MyDummyApplicationTest.java
@@ -1,8 +1,8 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.mockito.stubbing.OngoingStubbing;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -10,20 +10,26 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class MyDummyApplicationTest
 {
+    private MyDummyApplication myDummyApplication;
+    private DummyService dummyService;
 
-    MyDummyApplication myDummyApplication = new MyDummyApplication();
+    @Before
+    public void setUp(){
+        myDummyApplication = new MyDummyApplication();
+        dummyService = mock(DummyService.class);
+        myDummyApplication.setDummyService(dummyService);
+    }
 
 
     /**
-     * NOTE 1 :
+     * Example 1 :
      * when a parameterised method is mocked for a return value then the parameter
      * should be same value during exeution to get the same return value
      *
      */
     @Test
     public void myRun(){
-        DummyService dummyService = mock(DummyService.class);
-        myDummyApplication.setDummyService(dummyService);
+
         when(dummyService.run("test")).thenReturn(1);
         Assert.assertEquals(1,myDummyApplication.run("test"));
     }
