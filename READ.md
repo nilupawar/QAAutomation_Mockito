c15205dbf1ca048171ad8ce7c4fe2614afeafa79
Nilesh Pawar <nilesh.p.pawar@gmail.com>
2019-08-17 16:05:51 +0100

    Added different mockito verify

--- READ.md
+++ READ.md
@@ -1,3 +1,7 @@
 MyDummyApplicationTest
 Example 1 :- Returned value of the mocked method will be same only when sent parameter is same
 Example 2 :- Order of the method call is verified using inOrder.Verify methods sequence.
+
+
+MockitoAnnotation : This test class is created to show how to run test using Mockito annotations
+Example 1 :- Use of @InjectMocks @Mock tags
--- src/test/java/MockitoAnnotation.java
+++ src/test/java/MockitoAnnotation.java
@@ -5,7 +5,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.*;
 
 
 @RunWith(MockitoJUnitRunner.class)
@@ -20,8 +20,12 @@ public class MockitoAnnotation {
 
     @Test
     public void example1(){
-        doReturn(1).when(dummyService).run("test");
-        Assert.assertEquals(1,myDummyApplication.run("test"));
+        final String PARAM = "test";
+        doReturn(1).when(dummyService).run(PARAM);
+        Assert.assertEquals(1,myDummyApplication.run(PARAM));
+        verify(dummyService).run(PARAM);                                // Verifies method is called
+        verify(dummyService,times(1)).run(PARAM);  // Verifies method is called 1 time
+        verify(dummyService,atLeast(1)).run(PARAM);  // Verifies method is called atleast 1 time
     }
 
 }
